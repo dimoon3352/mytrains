fc09d1096b4ab2e7f622085206edc09b58632845
Me! <clashrepeir@mail.ru>
2025-02-05 17:40:47 +0300

    svg were added in (tabs)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,26 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+
+
+import CalcSVG from '../navigation/CalcSVG';
+import HomeSVG from '../navigation/HomeSVG';
+import ExercisesSVG from '../navigation/ExercisesSVG';
+import TrainsSVG from '../navigation/TrainsSVG';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+
+  const tabDarkTheme = {
+    bg: "#242424",
+    element: "#16A34A"
+  }
+
+  const tabLightTheme = {
+    bg: "#fff",
+    element: "#16A34A"
+  }
 
   return (
     <Tabs
@@ -17,33 +28,39 @@ export default function TabLayout() {
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: tabDarkTheme.bg, // add theme
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: 56,
+          paddingTop: 8,
+          paddingBottom: 8,
+        }
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          headerTitle: "index",
-          title: "index",
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: () => <HomeSVG color='#16A34A' size='40px' />,
         }}
       />
       <Tabs.Screen
-        name="trains"
+        name="Trains"
         options={{
-          headerTitle: "trains",
-          title: "trains",
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: () => <TrainsSVG color='#16A34A' size='40px' />,
         }}
       />
       <Tabs.Screen
-        name="exercises"
+        name="Exercises"
         options={{
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: () => <ExercisesSVG color='#16A34A' size='40px' />,
         }}
       />
       <Tabs.Screen
-        name="calculators"
+        name="Calculators"
         options={{
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: () => <CalcSVG color='#16A34A' size='40px' />,
         }}
       />
     </Tabs>
